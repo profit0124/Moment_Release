85ef91146a62a362ff0ae00fecea4599222d785a
profit0124 <profit0124@gmail.com>
2022-12-26 15:35:26 +0900

    Update ContentView.swift

--- Moment_Release/ContentView.swift
+++ Moment_Release/ContentView.swift
@@ -38,7 +38,7 @@ struct ContentView: View {
                     }
                 }
             }
-            Text("Select an itemsㄴㄴ")
+            Text("Select an itemsasdfㄴㄴ")
         }
     }
 
